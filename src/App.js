d9efd8ea5aad754e5040edb2aa85579733da0278
Tai Nguyen <nghientai@gmail.com>
2020-03-06 22:23:11 -0800

    Modified: App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import NavBar from "./components/navBar";
 class App extends Component {
   state = {
     counters: [
-      { id: 1, value: 3 },
+      { id: 1, value: 0 },
       { id: 2, value: 0 },
       { id: 3, value: 0 },
       { id: 4, value: 0 }
