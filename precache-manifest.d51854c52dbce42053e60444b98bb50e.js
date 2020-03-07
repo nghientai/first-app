ec6410856e24694229b84a34645256e9254b5b21
Tai Nguyen <nghientai@gmail.com>
2020-03-06 23:10:51 -0800

    Updates

---
+++ precache-manifest.d51854c52dbce42053e60444b98bb50e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bd9b168dfba5f006a6b8d176eb5057f",
+    "url": "/first-app/index.html"
+  },
+  {
+    "revision": "f7e9703db510f559ad6a",
+    "url": "/first-app/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "f7e9703db510f559ad6a",
+    "url": "/first-app/static/js/2.6322f8fd.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/first-app/static/js/2.6322f8fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd7c12d0d8e3ed784ffe",
+    "url": "/first-app/static/js/main.fee84f4b.chunk.js"
+  },
+  {
+    "revision": "d80f1eabfaa9eb6508bf",
+    "url": "/first-app/static/js/runtime-main.eb71c001.js"
+  }
+]);
